348170b1833376dc0f02062ef736c78ee1796d4e
Arlindo Viana <arlindoviana@gmail.com>
2019-02-25 18:16:05 -0300

    Initial commit
    1º commit

---
+++ sij-content/sij-content-platform-jar/src/main/java/com/vconsulte/sij/content/model/SijContentModel.java
@@ -0,0 +1,36 @@
+package com.vconsulte.sij.content.model;
+
+/**
+ * Created by avmcf, V&C Consultoria on 13/02/19.
+ */
+
+public interface SijContentModel {
+	
+    // Types
+    public static final String NAMESPACE_SGJ_CONTENT_MODEL  = "http://www.vconsulte.sij.com/model/content/2.0";
+    public static final String TYPE_SIJ_DOC = "doc";
+    public static final String TYPE_SIJ_DOCUMENTO = "Documento";
+
+    // Aspects
+    public static final String ASPECT_SIJ_PUBLICACAO = "Publicacao";
+    public static final String ASPECT_SIJ_DOCJURIDICO = "Documento Juridico";
+
+    // Properties
+    public static final String PROP_DOCTIPO = "Tipo";   
+    public static final String PROP_DOCSITUACAO = "Stituacao";						
+    public static final String PROP_DOCATIVO = "Ativo?";	
+    public static final String PROP_DOCDTATIVACAO = "Data Ativacao";
+    
+    public static final String PROP_PUBDTEDICAO = "Edicao";
+    public static final String PROP_PUBTRIBUNAL = "Tribunal";
+    public static final String PROP_PUBTOKEN = "Tokens";
+    
+    public static final String PROP_JURNUMPROCESSO = "Procesoo";
+    public static final String PROP_JURTRIBUNAL = "Tribunal";
+    public static final String PROP_JURVARA = "Vara";
+    public static final String PROP_JURMUNICIPIO = "Vara";
+
+    // Associations
+    public static final String ASSN_DOCUMENTOS_RELACIONADOS = "Documentos Relacionados";
+	
+}
